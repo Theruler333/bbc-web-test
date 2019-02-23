37a6a15f5c64da77664ff2eb7c7da33317b06872
Theruler333 <harveypeachey@hotmail.co.uk>
2019-02-23 13:43:10 +0000

    Rearranged file structure and added sticky header component

---
+++ src/components/StickyHeader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './styles/StickyHeader.css';
+
+const StickyHeader = ({}) => {
+  return (
+    <header className="App-header" aria-label="Peachey News">
+      <img src={logo} className="App-logo" alt="logo" />
+    </header>
+  )
+}
+
+export default StickyHeader;
