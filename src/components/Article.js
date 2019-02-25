a87538d4002ac529b56a5cbd00a015ad1a0ff330
Theruler333 <harveypeachey@hotmail.co.uk>
2019-02-25 12:37:57 +0000

    Added static article heading component

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Heading from './article-templates/Heading';
 import './styles/Article.css';
 
 const Article = () => {
   return (
     <article>
-      <h1>I'm an article</h1>
+      <h1>I am the Article Component</h1>
+      <Heading/>
     </article>
   )
 }
---
+++ src/components/article-templates/Heading.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './../styles/article-template-styles/Heading.css';
+
+const Heading = () => {
+  return (
+      <h2>I am the static heading template component of an article</h2>
+  )
+}
+
+export default Heading;
