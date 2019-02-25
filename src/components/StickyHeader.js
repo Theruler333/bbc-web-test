305dde7c7e8bf8be06cd6a426663fdbda62c7073
Theruler333 <harveypeachey@hotmail.co.uk>
2019-02-25 23:39:02 +0000

    Edited generic parts of the website like favicon, title

--- src/components/StickyHeader.js
+++ src/components/StickyHeader.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ArticleNavigator from './ArticleNavigator';
-import logo from './logo.svg';
 import './styles/StickyHeader.css';
 
 const StickyHeader = ({handleArticleNav, currentArticle, amountOfArticles}) => {
   return (
     <header className="Header" aria-label="Peachey News">
-      <img src={logo} className="App-logo" alt="logo" />
+      <p>Peachey News</p>
       <ArticleNavigator handleArticleNav={handleArticleNav}
       currentArticle={currentArticle}
       amountOfArticles={amountOfArticles}
