4f0d0f66f677bc71582fc4fec8b11923e9a5c809
Theruler333 <harveypeachey@hotmail.co.uk>
2019-02-25 19:34:56 +0000

    Fetched article data with a promise and fixed cors issue using raw data from GitHub

--- src/components/App.js
+++ src/components/App.js
@@ -10,22 +10,39 @@ class App extends Component {
     this.state = {
       articleData: [],
       articleURLS: [
-        "https://github.com/bbc/news-coding-test-dataset/blob/master/data/article-1.json",
-        "https://github.com/bbc/news-coding-test-dataset/blob/master/data/article-2.json",
-        "https://github.com/bbc/news-coding-test-dataset/blob/master/data/article-3.json",
-        "https://github.com/bbc/news-coding-test-dataset/blob/master/data/article-4.json",
-        "https://github.com/bbc/news-coding-test-dataset/blob/master/data/article-5.json"
+        "https://raw.githubusercontent.com/bbc/news-coding-test-dataset/master/data/article-1.json",
+        "https://raw.githubusercontent.com/bbc/news-coding-test-dataset/master/data/article-2.json",
+        "https://raw.githubusercontent.com/bbc/news-coding-test-dataset/master/data/article-3.json",
+        "https://raw.githubusercontent.com/bbc/news-coding-test-dataset/master/data/article-4.json",
+        "https://raw.githubusercontent.com/bbc/news-coding-test-dataset/master/data/article-5.json"
       ],
       randomArticleUrls: []
     };
   }
 
   componentDidMount() {
+    // Randomises article order
     for (var i = 0; i < (this.state.randomArticleUrls.length+this.state.articleURLS.length); i++) {
       this.state.randomArticleUrls.push(
         this.state.articleURLS.splice((Math.random()*this.state.articleURLS.length),1)
       );
     }
+    // Fetches article data using a promise
+    Promise.all([
+      fetch(this.state.randomArticleUrls[0]),
+      fetch(this.state.randomArticleUrls[1]),
+      fetch(this.state.randomArticleUrls[2]),
+      fetch(this.state.randomArticleUrls[3]),
+      fetch(this.state.randomArticleUrls[4])
+    ])
+    .then(([res1, res2, res3, res4, res5]) => Promise.all([res1.json(), res2.json(), res3.json(), res4.json(), res5.json()]))
+    .then(([data1, data2, data3, data4, data5]) => {
+        this.setState({articleData:[data1, data2, data3, data4, data5]});
+    })
+    .catch((error) => {
+      console.log(error)
+      console.log("Article source data not availible")
+    });
   }
 
   render() {
