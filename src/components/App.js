525f3645c32ec4f9438e90f469cf0ceb8cfc9f03
Theruler333 <harveypeachey@hotmail.co.uk>
2019-02-25 16:30:21 +0000

    Added article randomiser logic

--- src/components/App.js
+++ src/components/App.js
@@ -5,26 +5,35 @@ import Ranker from './Ranker';
 import './styles/App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      articleData: [],
+      articleURLS: [
+        "https://github.com/bbc/news-coding-test-dataset/blob/master/data/article-1.json",
+        "https://github.com/bbc/news-coding-test-dataset/blob/master/data/article-2.json",
+        "https://github.com/bbc/news-coding-test-dataset/blob/master/data/article-3.json",
+        "https://github.com/bbc/news-coding-test-dataset/blob/master/data/article-4.json",
+        "https://github.com/bbc/news-coding-test-dataset/blob/master/data/article-5.json"
+      ],
+      randomArticleUrls: []
+    };
+  }
+
+  componentDidMount() {
+    for (var i = 0; i < (this.state.randomArticleUrls.length+this.state.articleURLS.length); i++) {
+      this.state.randomArticleUrls.push(
+        this.state.articleURLS.splice((Math.random()*this.state.articleURLS.length),1)
+      );
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <StickyHeader/>
         <Article/>
         <Ranker/>
-        {/*<header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>*/}
       </div>
     );
   }
